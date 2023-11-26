ee5235dc3e547b58c18730ea19fc66e4742d1d20
primeDevansh <dvcam.rastogi1980@gmail.com>
2023-11-27 00:14:07 +0530

    RT1 of Ass1 But, done w/o Group()

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,15 +8,50 @@
 import SwiftUI
 
 struct ContentView: View {
-    let emojis: Array<String> = ["✈️", "🚗", "🚀", "🚙"]
+    @State var CardCount: Int = 4
+    let emojis: Array<String> = ["✈️", "🚗", "🚀", "🚙", "🚎", "🚛", "🏎", "🛫", "🚕", "🚐", "⛴", "🚝", "🚟"]
     var body: some View {
-        HStack {
-            ForEach(emojis.indices, id: \.self) { index in
-                CardView(content: emojis[index])
+        VStack {
+            ScrollView() {
+                cards
             }
+            Spacer()
+            HStack {
+                cardRemover
+                Spacer()
+                cardAdder
+            }
+            .imageScale(.large)
+            .font(.largeTitle)
         }
         .padding()
+    }
+    
+    var cards: some View {
+        LazyVGrid(columns: [GridItem(.adaptive(minimum: 120))]) {
+            ForEach(0..<CardCount, id: \.self) { index in
+                CardView(content: emojis[index])
+                    .aspectRatio(2/3, contentMode: .fit)
+            }
+        }
         .foregroundColor(.orange)
+    }
+    
+    var cardAdder: some View {
+        cardCountAdjust(by: +1, symbol: "rectangle.stack.badge.plus.fill")
+    }
+    
+    var cardRemover: some View {
+        cardCountAdjust(by: -1, symbol: "rectangle.stack.badge.minus.fill")
+    }
+    
+    func cardCountAdjust(by offset: Int, symbol: String) -> some View {
+        Button(action: {
+            CardCount += offset
+        }, label: {
+            Image(systemName: symbol)
+        })
+        .disabled(CardCount + offset < 1 || CardCount + offset > emojis.count )
     }
 }
 
