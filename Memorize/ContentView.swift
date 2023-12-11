eebb74ec21c0093cb10973677d56697aefed97e3
primeDevansh <dvcam.rastogi1980@gmail.com>
2023-12-11 13:47:09 +0530

    Made it comfortable for lecture 3's demo

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,7 +8,6 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var CardCount: Int = 4
     let emojis: Array<String> = ["✈️", "🚗", "🚀", "🚙", "🚎", "🚛", "🏎", "🛫", "🚕", "🚐", "⛴", "🚝", "🚟"]
     
     var body: some View {
@@ -16,48 +15,18 @@ struct ContentView: View {
             ScrollView() {
                 cards
             }
-            Spacer()
-            cardCountAdjusters
         }
         .padding()
     }
     
-    var cardCountAdjusters: some View {
-        HStack {
-            cardRemover
-            Spacer()
-            cardAdder
-        }
-        .imageScale(.large)
-        .font(.largeTitle)
-
-    }
-    
     var cards: some View {
-        LazyVGrid(columns: [GridItem(.adaptive(minimum: 150))]) {
-            ForEach(0..<CardCount, id: \.self) { index in
+        LazyVGrid(columns: [GridItem(.adaptive(minimum: 90))]) {
+            ForEach(emojis.indices, id: \.self) { index in
                 CardView(content: emojis[index])
                     .aspectRatio(2/3, contentMode: .fit)
             }
         }
         .foregroundColor(.orange)
-    }
-    
-    var cardAdder: some View {
-        cardCountAdjust(by: +1, symbol: "rectangle.stack.badge.plus.fill")
-    }
-    
-    var cardRemover: some View {
-        cardCountAdjust(by: -1, symbol: "rectangle.stack.badge.minus.fill")
-    }
-    
-    func cardCountAdjust(by offset: Int, symbol: String) -> some View {
-        Button(action: {
-            CardCount += offset
-        }, label: {
-            Image(systemName: symbol)
-        })
-        .disabled(CardCount + offset < 1 || CardCount + offset > emojis.count )
     }
 }
 
